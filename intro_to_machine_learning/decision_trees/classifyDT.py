d3cf8da3f5b68bb892cba63570c5dc9b9cd26b1e
Venkat Malladi <vsmalladi@gmail.com>
2017-01-16 16:11:46 -0600

    update syntax.

--- intro_to_machine_learning/decision_trees/classifyDT.py
+++ intro_to_machine_learning/decision_trees/classifyDT.py
@@ -1,7 +1,8 @@
 def classify(features_train, labels_train):
 
     ### your code goes here--should return a trained decision tree classifer
-
-
+    from sklearn import tree
+    clf = tree.DecisionTreeRegressor()
+    clf = clf.fit(features_train, labels_train)
 
     return clf
