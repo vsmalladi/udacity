daecd58ef1d33e56c0ad5dcbcf7dba1696660563
Venkat Malladi <vsmalladi@gmail.com>
2017-03-19 10:18:21 -0500

    First model of decision tree classifier.

---
+++ intro_to_machine_learning/decision_trees/dt_author_id.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+    Use a Decision Tree to identify emails from the Enron corpus by author:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred.astype(int))
+print "Accuracy: ", acc
+#########################################################
